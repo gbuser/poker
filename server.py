c0316e814cb37a519382127ae5be4cbee174b420
Gregory <gregory.buser@gmail.com>
2024-01-22 13:36:33 -0600

    synch server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from poker_utils import *
 SERVER = ""
 PORT = 5050
 ADDR = (SERVER, PORT)
@@ -28,8 +29,16 @@ def handle_client(conn, addr):
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode('utf-8')
             print(f'{addr}: {msg}')
-            conn.send("You are connected to basic poker server".encode(FORMAT))
             conn.send("care to play some poker?".encode(FORMAT))
+            conn.send("you've been dealt:\n".encode(FORMAT))
+            deck = make_deck()
+            random.shuffle(deck)
+            hand = deal_cards(5, deck)
+            hand = sort_hand(hand)
+            msg = str(data[hand]['tier0'])
+            conn.send(string_hand(hand).encode(FORMAT))
+            print(msg)
+            conn.send(msg.encode(FORMAT))
             if msg == DISCONNECT:
                 connected = False
                 conn.close()
